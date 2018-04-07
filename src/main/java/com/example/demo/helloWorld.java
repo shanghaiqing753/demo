2a5c8c2f16cd87d615a5cc6fc8d38ddf30fa13d5
shanghaiqing753 <718011217@qq.com>
2018-04-08 00:33:10 +0800

    提交

---
+++ src/main/java/com/example/demo/helloWorld.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+/**
+ * Created by 清风岁月 on 2018/4/2
+ */
+
+public class helloWorld {
+}
---
+++ src/main/java/com/it/test/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.it.test;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
+}
